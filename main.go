1ccca55eb0e2f255d69a6739825bc886b5ca0fe0
Daniel Volchek <danielvolchek123@gmail.com>
2023-07-06 13:46:42 -0400

    started server

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/danielvolchek/stim-core/pkg/server"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+
+	fmt.Println("Hello, Stim-Core")
+
+	godotenv.Load(".env")
+
+	server.StartHttpServer()
 
 }
